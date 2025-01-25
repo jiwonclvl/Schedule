3b81018a4c73006af29e331b5be4c6d81cd4afb1
abc <clvlajrrhtlvek@gmail.com>
2025-01-26 05:18:25 +0900

    ìinitial setting complete

--- src/main/java/com/example/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/schedule/controller/ScheduleController.java
@@ -13,35 +13,5 @@ import java.util.*;
 @RestController
 @RequestMapping("/schedules")
 public class ScheduleController {
-    //빈 MAP 배열 자료구조 생성
-   private final Map<Long, Schedule> scheduleList = new HashMap<>();
-
-   //일정 생성하기
-   @PostMapping
-    public ResponseEntity<ScheduleResponseDto> createSchedule(@RequestBody ScheduleRequestDto dto) {
-
-       //id 1씩 증가
-       Long scheduleId = scheduleList.isEmpty() ? 1: Collections.max(scheduleList.keySet()) +1;
-
-       //아이디, 할일, 작성자명, 비밀번호, 작성일, 생성일 Map에 저장 --> 나중에 데이터 베이스에 저장해야함
-       Schedule schedule = new Schedule(scheduleId, dto.getTodo(), dto.getAuthor(), dto.getPassword(), dto.getCreateAt(),dto.getCreateAt());
-       scheduleList.put(scheduleId, schedule);
-
-       return new ResponseEntity<>(new ScheduleResponseDto(schedule),HttpStatus.CREATED);
-   }
-
-   //전체 일정 조회하기
-    @GetMapping
-    public ResponseEntity<List<ScheduleResponseDto>> findAllSchedules() {
-       List<ScheduleResponseDto> scheduleResponseDtoList = new ArrayList<>();
-
-       //배열에 추가
-       for(Schedule schedule : scheduleList.values()) {
-           scheduleResponseDtoList.add(new ScheduleResponseDto(schedule));
-       }
-
-       return new ResponseEntity<>(scheduleResponseDtoList, HttpStatus.OK);
-    }
-
 
 }
