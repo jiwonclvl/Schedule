1a877c444342680f7a84f723b90edbd4651fe023
abc <clvlajrrhtlvek@gmail.com>
2025-01-27 17:53:03 +0900

    Docs: "Swagger 문서 수정"
    각 메서드에 대한 상세 설명을 추가하여 사용자가 문서를 통해 프로젝트를
    쉽게 이해할 수 있도록 하였다.

--- src/main/java/com/example/schedule/docs/ControllerDocs.java
+++ src/main/java/com/example/schedule/docs/ControllerDocs.java
@@ -17,16 +17,18 @@ import java.util.List;
 @Tag(name = "일정 관리", description = "일정 관리 API입니다.")
 public interface ControllerDocs {
 
-    @Operation(summary = "일정 정보 저장", description = "일정을 저장합니다.")
+    //일정 등록
+    @Operation(summary = "일정 정보 저장", description = "할일, 작성자명, 비밀번호를 입력하여 일정을 저장합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "일정 저장 성공"),
-            @ApiResponse(responseCode = "405", description = "일정 저장 실패(정보 기입 X)") })
+            @ApiResponse(responseCode = "404", description = "일정 정보 미입력으로 저장 실패") })
     public ResponseEntity<ScheduleResponseDto> createSchedule(@RequestBody ScheduleRequestDto dto);
 
-    @Operation(summary = "전체 일정 조회", description = "전체 일정을 조회합니다.")
+    //일정 전체 조회
+    @Operation(summary = "전체 일정 조회", description = "작성자명, 날짜 조건에 따른 전체 일정을 조회합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "일정 전체 조회 성공"),
-            @ApiResponse(responseCode = "400", description = "일정 전체 조회 실패") })
+            @ApiResponse(responseCode = "404", description = "일정 전체 조회 실패") })
     public ResponseEntity<List<ScheduleResponseDto>> findAllSchedules(@RequestParam(required = false) String author,
                                                                       @RequestParam(required = false) String update);
 
